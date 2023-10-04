eb9dfb830b9d0382f63d99ca0dd4602252058cdd
farrelaranku <114351382+farrelaranku@users.noreply.github.com>
2023-10-04 12:49:23 +0700

    Delete resources/views/create.blade.php

--- resources/views/create.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/create.blade.php -->
-<form method="POST" action="{{ route('seed-data') }}" enctype="multipart/form-data">
-    @csrf
-    <input type="text" name="name" placeholder="Name">
-    <input type="text" name="address" placeholder="Address">
-    <input type="text" name="phone" placeholder="Phone">
-    <input type="file" name="image" accept="image/*">
-    <button type="submit">Seed Data</button>
-</form>
